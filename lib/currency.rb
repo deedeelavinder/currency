03866c5e575772d3ce216bdbe24afa9c2bf592a3
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-09-24 18:55:22 -0400

    test 3 successful

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,8 +1,19 @@
 class Currency
-  attr_accessor :sym :num
+  include Comparable
+  attr_reader :currency, :amount
 
-  def initialize(sym, num)
-    @sym = sym
-    @num = num
+  def initialize(currency, amount)
+    @currency = currency
+    @amount = amount
   end
+
+  def <=>(compared_object)
+    @amount <=> compared_object.amount
+  end
+
+  def add(added_amt)
+    new_amount = @amount + added_amt.amount
+    Currency.new(@currency, new_amount)
+  end
+  
 end
--- test/currency_test.rb
+++ test/currency_test.rb
@@ -4,10 +4,21 @@ require "currency"
 class CurrencyTest < MiniTest::Unit::TestCase
 
 def test_for_amount_and_code
-  cur_curr = Currency.new ($, 5)
-  assert true, cur_curr = ($, 5)
+  money = Currency.new(:USD, 5)
+  assert_equal money.currency, :USD
+  assert_equal money.amount, 5
 end
-def test_parameters_for_currency
+
+def test_another_currency_object_that_equals_the_first
+  money = Currency.new(:USD, 5)
+  money1 = Currency.new(:USD, 5)
+  assert_equal money, money1
+end
+
+def test_can_add_one_amount_to_another_with_same_currency
+  money = Currency.new(:USD, 5)
+  money1 = Currency.new(:USD, 10)
+  assert_equal Currency.new(:USD, 15), money.add(money1)
 end
 
 end
