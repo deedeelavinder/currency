4de2bb611e7ff64046cd118c9eb2b526054d26f1
DeeDee Lavinder <deedeelavinder@gmail.com>
2014-09-24 20:09:45 -0400

    Finished 5th task

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,3 +1,6 @@
+class DifferentCurrencyCodeError < RuntimeError
+end
+
 class Currency
   include Comparable
   attr_reader :currency, :amount
@@ -9,11 +12,25 @@ class Currency
 
   def <=>(compared_object)
     @amount <=> compared_object.amount
+    @currency <=> compared_object.currency
   end
 
-  def add(added_amt)
-    new_amount = @amount + added_amt.amount
-    Currency.new(@currency, new_amount)
+  def add(add_amt)
+    if add_amt.currency != @currency
+      raise DifferentCurrencyCodeError, "Your currencies must match."
+    else
+      new_amount = @amount + add_amt.amount
+      Currency.new(@currency, new_amount)
+    end
   end
-  
+
+  def subtract(sub_amt)
+    if sub_amt.currency != @currency
+      raise DifferentCurrencyCodeError, "Your currencies must match."
+    else
+      new_amount = @amount - sub_amt.amount
+      Currency.new(@currency, new_amount)
+    end
+  end
+
 end
--- test/currency_test.rb
+++ test/currency_test.rb
@@ -16,9 +16,30 @@ def test_another_currency_object_that_equals_the_first
 end
 
 def test_can_add_one_amount_to_another_with_same_currency
-  money = Currency.new(:USD, 5)
-  money1 = Currency.new(:USD, 10)
-  assert_equal Currency.new(:USD, 15), money.add(money1)
+  money = Currency.new(:USD, 10)
+  money1 = Currency.new(:USD, 15)
+  assert_equal Currency.new(:USD, 25), money.add(money1)
 end
 
+def test_can_subtract_one_amount_to_another_with_same_currency
+  money = Currency.new(:USD, 25)
+  money1 = Currency.new(:USD, 10)
+  assert_equal Currency.new(:USD, 15), money.subtract(money1)
+end
+
+def test_gives_error_message_if_currencies_being_added_do_not_match
+  assert_raises DifferentCurrencyCodeError do
+  money = Currency.new(:USD, 10)
+  money1 = Currency.new(:DKK, 15)
+  money.add(money1)
+  end
+end
+
+def test_gives_error_message_if_currencies_being_subtracted_do_not_match
+  assert_raises DifferentCurrencyCodeError do
+  money = Currency.new(:USD, 20)
+  money1 = Currency.new(:DKK, 15)
+  money.subtract(money1)
+  end
+end
 end
